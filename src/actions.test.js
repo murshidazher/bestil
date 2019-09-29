55c1ad429eb7947c9af79a1d55e4117cbe0a6a72
murshidazher <murshidazher@yahoo.com>
2019-09-29 14:00:28 +0530

    feat(fetch): Add fetch test

--- src/actions.test.js
+++ src/actions.test.js
@@ -9,6 +9,7 @@ import * as actions from './actions';
 
 import configureMockStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
+import fetchMock from 'fetch-mock';
 
 const mockStore = configureMockStore([thunkMiddleware]);
 
@@ -36,5 +37,75 @@ describe('requestBooks', () => {
         }
 
         expect(action[0]).toEqual(expectedAction)
+    });
+
+
+    it('expected actions should be dispatched on successful request', () => {
+
+        const store = mockStore();
+        const expectedActions = [
+            { type: REQUEST_BOOKS_PENDING },
+            { type: REQUEST_BOOKS_SUCCESS }
+        ]
+
+        // Mock the fetch() global to always return the same value for GET
+        // requests to all URLs.
+        fetchMock.get('*', { response: 200 })
+
+            console.log(store.dispatch(actions.requestBooks('abc')))
+            return store.dispatch(actions.requestBooks('John'))
+                .then(() => {
+                    const action = store.getActions()
+                    expect(action).toEqual(expectedActions)
+                })
+
+        fetchMock.restore()
     })
+    
+    it('expected actions should be dispatched on failed request', () => {
+        const store = mockStore()
+        const expectedActions = [
+            { type: REQUEST_BOOKS_PENDING },
+            { type: REQUEST_BOOKS_FAILED }
+        ]
+        // Mock the fetch() global to always return the same value for GET
+        // requests to all URLs.
+        fetchMock.get('*', { response: 404 })
+
+        return store.dispatch(actions.requestBooks('John'))
+        .then(() => {
+            const action = store.getActions()
+            expect(action).toEqual(expectedActions)
+        })
+
+        fetchMock.restore()
+    })
+    
+
+        
+    
+
+        // const expectedActions = [
+        //     { type: REQUEST_BOOKS_PENDING },
+        //     {
+        //         type: REQUEST_BOOKS_SUCCESS,
+        //         payload: [{
+        //                 id: 1,
+        //                 volumeInfo: {
+        //                     title: 'Book about John Doe',
+        //                     authors: ['Jane Jackson', 'Janet Jackson'],
+        //                     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nobis labore fuga',
+        //                     imageLinks: {
+        //                         smallThumbnail:
+        //                             'https://books.google.com/books/content?id=SQMQQyIaACYC&amp;printsec=frontcover&amp;img=1&amp;zoom=5&amp;source=gbs_api'
+        //                     }
+        //                 }
+        //             }]
+        //     },
+        //     {
+        //         type: REQUEST_BOOKS_FAILED,
+        //         payload: 'An unexpected error occurred' 
+        //     }
+        // ]
+        
 })
